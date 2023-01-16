5b934d454cc823721c58459f55325d0f1122e4ac
FoundIzalith <53011549+FoundIzalith@users.noreply.github.com>
2023-01-16 15:49:11 -0500

    Fix include errors; mistake in checking argv len()

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 import lmu  # https://github.com/hrshtv/pytorch-lmu
 import torch
 import torchaudio
-import langIdentifier
+from langIdentifier import audioData, languageIdentifier
 from torch.utils.data import random_split
-import audioData
 import sys 
-import os.path 
+from os.path import exists
 
 def main():
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         datapath = "./data/"
     else:
         datapath = sys.argv[1]
 
-    if not path.exists(datapath):
-        printf("Error: datapath not found")
+    if not exists(datapath):
+        print("Error: datapath not found")
         exit()
 
-    transform = transforms.ToTensor()
     dataset= audioData(dataFrame, "./data/")
     train_size = int(0.8 * len(dataset))
     val_size = len(dataset) - train_size
     
     audio_train, audio_val = random_split(dataset, [train_size, val_size])
-    
 
     perm = torch.load("permutation.pt").long() # created using torch.randperm(784)
     ds_train = psMNIST(audio_train, perm)
