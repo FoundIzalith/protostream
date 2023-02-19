de6bc801bc2340b50de1aaf1f6fba3149b01b6af
FoundIzalith <53011549+FoundIzalith@users.noreply.github.com>
2023-02-18 23:33:18 -0500

    Adjust model, add training loop

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ N_x = 2 # Dimension of input
 N_c = 176 # Number of classes, 176 for 176 languages
 N_h = 212 # hidden layer size
 N_m = 256 # memory size
+N_t = 784 # Number of time steps
 batch = 100 # batch size
 N_epochs = 10
 
@@ -34,6 +35,12 @@ def main():
         print("Error: metadata not found")
         exit()
 
+    if torch.cuda.is_available():
+        DEVICE = "cuda"
+        torch.cuda.empty_cache()
+    else:
+        DEVICE = "cpu"
+
     dataFrame = pandas.read_csv(metadata)
     dataFrame.head()
 
@@ -46,14 +53,45 @@ def main():
     dl_train = DataLoader(audio_train, batch_size = batch, shuffle = True, num_workers = 2)
     dl_val   = DataLoader(audio_val, batch_size = batch, shuffle = True, num_workers = 2)
 
-    model = Model(
-        input_size = N_x #Dimensioon of input
-        output_size = N_c #Number of classes # 176 for 176 languages 
-        hidden_size = N_h #Dimension of hidden state
-        memory_size =  N_m #dimension of memory
-        learn_a = true
-        learn_b = true    
+    model = languageIdentifier(
+        input_size = N_x, # Dimension of input
+        output_size = N_c, #Number of classes # 176 for 176 languages 
+        hidden_size = N_h, #Dimension of hidden state
+        memory_size =  N_m, #dimension of memory
+        theta = N_t,
+        learn_a = True,
+        learn_b = True    
     )
+    model = model.to(DEVICE)
+
+    countParameters(model)
+    optimizer = optim.Adam(params = model.parameters())
+
+    criterion = nn.CrossEntropyLoss()
+    criterion = criterion.to(DEVICE)
+
+    train_losses = []
+    train_accs = []
+    val_losses = []
+    val_accs = []
+
+    for epoch in range(N_epochs):
+            print("Epoch ", epoch)
+
+            train_loss, train_acc = train(model, dl_train, optimizer, criterion)
+            val_loss, val_acc = validate(model, dl_val, criterion)
+
+            train_losses.append(train_loss)
+            train_accs.append(train_acc)
+            val_losses.append(val_loss)
+            val_accs.append(val_acc)
+
+            print(f"Train Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%")
+            print(f"Val. Loss: {val_loss:.3f} |  Val. Acc: {val_acc*100:.2f}%")
+            print()
+     
+
+
         
 if __name__ == "__main__":
     main()
