6482498dd58fdddc964ceb44e52e55c811610a4c
FoundIzalith <53011549+FoundIzalith@users.noreply.github.com>
2023-02-18 23:21:15 -0500

    Add model parameters, global variables

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from os.path import exists
 import pandas 
 from pathlib import Path
 
+N_x = 2 # Dimension of input
+N_c = 176 # Number of classes, 176 for 176 languages
+N_h = 212 # hidden layer size
+N_m = 256 # memory size
+batch = 100 # batch size
+N_epochs = 10
+
 def main():
     if len(sys.argv) == 1:
         datapath = Path.cwd()/'data'
@@ -36,8 +43,17 @@ def main():
     
     audio_train, audio_val = random_split(dataset, [train_size, val_size])
 
-    dl_train = DataLoader(audio_train, batch_size = N_b, shuffle = True, num_workers = 2)
-    dl_val   = DataLoader(audio_val, batch_size = N_b, shuffle = True, num_workers = 2)
+    dl_train = DataLoader(audio_train, batch_size = batch, shuffle = True, num_workers = 2)
+    dl_val   = DataLoader(audio_val, batch_size = batch, shuffle = True, num_workers = 2)
+
+    model = Model(
+        input_size = N_x #Dimensioon of input
+        output_size = N_c #Number of classes # 176 for 176 languages 
+        hidden_size = N_h #Dimension of hidden state
+        memory_size =  N_m #dimension of memory
+        learn_a = true
+        learn_b = true    
+    )
         
 if __name__ == "__main__":
     main()
